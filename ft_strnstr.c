c7e200bdc1e9bb2b0e6305a608522e71aa714bd5
matusala3 <matusalakiflom@gmail.com>
2025-04-22 11:49:29 +0300

    ft_strnstr fixe again

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: magebreh <magebreh@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 16:47:20 by magebreh          #+#    #+#             */
-/*   Updated: 2025/04/22 10:49:22 by magebreh         ###   ########.fr       */
+/*   Updated: 2025/04/22 11:49:10 by magebreh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,15 @@ char *ft_strnstr(const char *haystack, const char *needle, size_t len)
         return ((char *)haystack);
     while (i < len && haystack[i])
     {
-        j = 0;
-        while ((i + j < len) && haystack[i + j] == needle[j])
+        if(haystack [i] == needle[0])
         {
-            if (needle[j + 1] == '\0')
+            j = 0;
+            while ((i + j < len) && haystack[i + j] == needle[j] &&  needle[j])
+                j++;
+            if (needle[j] == '\0')
                 return ((char *)&haystack[i]);
-            j++;
         }
         i++;
-        }        
+    }        
     return (NULL);
 }
